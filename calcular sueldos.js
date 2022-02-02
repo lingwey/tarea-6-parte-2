74b25badd12cefc0b39cec01f1f3cd5247352aa8
lingwey <nachozanetti0@gmail.com>
2022-02-02 16:49:00 -0300

    parte 2 de la ttarea 6

---
+++ calcular sueldos.js
@@ -0,0 +1,39 @@
+function salarioMensual (salario){
+    let contador = 0;
+    for (let i = 0; i < salario.length; i++){
+        contador += salario[i]
+    }
+    const mensual = contador / 12;
+    return mensual / salario.length;
+}
+
+function salarioAnual (salario){
+    let contador = 0;
+    for (let i = 0; i < salario.length; i++){
+        contador += salario[i]
+    }
+    return contador / salario.length;
+}
+
+
+function mayoSalario (salario){
+    let mayor = salario[0]
+    for (let i = 0; i < salario.length; i++){
+        if (mayor < salario[i]){
+            mayor = salario[i];
+        }
+    }
+    return mayor;
+}
+
+function menorSalario (){
+    let menor = salario[0];
+    for (let i = 0; i < salario.length; i++){
+        if (menor > salario[i]){
+            menor = salario[i];
+        }
+    }
+    return menor;
+}
+
+
---
+++ interaccion tarea 6 parte 2.js
@@ -0,0 +1,110 @@
+/*
+TAREA:
+Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
+Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
+
+Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
+*/
+
+document.querySelector('#agregar').onclick = function(event){
+    //let integrante = 0
+    //integrante +=1
+    crearIntegrante();
+    mostrarResetear();
+    mostrarCalcular();
+    event.preventDefault();
+}
+
+
+
+function crearIntegrante (){
+    const $div = document.createElement('div');
+    $div.className = '.familiar';
+
+    const $label = document.createElement('label');
+    $label.textContent = 'ingrese el salario anual #';
+
+    const $input = document.createElement('input');
+    $input.type = 'number';
+
+    $div.appendChild($label);
+    $div.appendChild($input)
+
+    const $ingresoSueldo = document.querySelector('#ingreso-sueldos');
+    $ingresoSueldo.appendChild($div);
+
+}
+document.querySelector('#calcular').onclick = function(event){
+    const sueldos = obtenerSueldos ();
+    mostrarSueldos ("mayor", mayoSalario(sueldos));
+    mostrarSueldos ("menor", menorSalario(sueldos));
+    mostrarSueldos ("promedioA", salarioAnual(sueldos));
+    mostrarSueldos ("promedioM", salarioMensual(sueldos));
+
+    console.log (sueldos)
+    esconderAgregar();
+    mostrarResultados ();
+    event.preventDefault();
+}
+
+document.querySelectorAll('#reiniciar').onclick = reiniciar();
+
+function reiniciar (){
+    mostrarAgregar();
+    esconderResultado ();
+    esconderCalcular();
+    esconderResetear();
+    borrarTodo ();
+}
+
+function mostrarResetear (){
+    document.querySelector('#reiniciar').className= '';
+}
+
+function mostrarCalcular (){
+    document.querySelector('#calcular').className = '';
+}
+
+function esconderResetear (){
+    document.querySelector('#reiniciar').className = 'escondido';
+}
+
+function esconderCalcular (){
+    document.querySelector('#calcular').className = 'escondido';
+}
+
+function mostrarResultados (){
+    document.querySelector('#resultados').className = '';
+}
+
+function esconderResultado (){
+    document.querySelector('#resultados').className = 'escondido';
+}
+
+function esconderAgregar (){
+    document.querySelector('#agregar').className = 'escondido';
+}
+
+function mostrarAgregar (){
+    document.querySelector('#agregar').className = '';
+}
+
+function borrarTodo (){
+    const $integrantes = document.querySelectorAll('.familiar');
+    for (let i = 0; i < $integrantes.length; i++){
+        $integrantes[i].remove();
+    }
+}
+
+function obtenerSueldos (){
+    const $sueldos = document.querySelectorAll('.familiar input');
+    const sueldos = [];
+    for (let i =0; i < $sueldos.length; i++){
+        sueldos.push(Number($sueldos[i].value));
+    }
+    return sueldos;
+}
+
+function mostrarSueldos (tipo, sueldo){
+    document.querySelector(`#${tipo}`).textContent = valor;
+}
